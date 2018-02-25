7e9b59d34261faff12695d931862b70d58a411d2
Eugene <chumakzhenya@yahoo.com>
2018-02-25 19:48:47 +0300

    + test for onCreateViewHolder async view provider

---
+++ app/src/test/java/com/eugene/cmcclient/ui/common/inflate_provider/AsyncProviderTest.kt
@@ -0,0 +1,16 @@
+package com.eugene.cmcclient.ui.common.inflate_provider
+
+import android.test.mock.MockContext
+import android.view.LayoutInflater
+import android.view.View
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+/**
+ * Created by Eugene on 25.02.2018.
+ */
+class AsyncProviderTest {
+}
